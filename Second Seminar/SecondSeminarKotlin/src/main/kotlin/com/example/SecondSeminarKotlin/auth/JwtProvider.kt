911d69db3010bf6b28d35224fbf6c1183cd84e87
sunseo18 <sunseo1818@gmail.com>
2023-12-07 17:27:29 +0900

    [코틀린/6주차] JWT 생성/검사 구현

---
+++ Second Seminar/SecondSeminarKotlin/src/main/kotlin/com/example/SecondSeminarKotlin/auth/JwtProvider.kt
@@ -0,0 +1,67 @@
+package com.example.SecondSeminarKotlin.auth
+
+import io.jsonwebtoken.*
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Component
+import java.util.*
+import javax.crypto.SecretKey
+
+private const val MEMBER_ID = "memberId"
+
+@Component
+class JwtProvider(
+    @Value("\${jwt.secret}")
+    private val JWT_SECRET: String
+) {
+
+    fun generateToken(authentication: Authentication, tokenExpirationTime: Long): String {
+        val now = Date()
+
+        val claims = Jwts.claims()
+            .setIssuedAt(now)
+            .setExpiration(Date(now.time + tokenExpirationTime))
+
+        claims.put(MEMBER_ID, authentication.principal)
+
+        return Jwts.builder()
+            .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+            .setClaims(claims)
+            .signWith(getSigningKey())
+            .compact()
+    }
+
+    private fun getSigningKey(): SecretKey {
+        val encodedKey = Base64.getEncoder().encodeToString(JWT_SECRET.toByteArray())
+        return Keys.hmacShaKeyFor(encodedKey.toByteArray())
+    }
+
+    private fun getBody(token: String): Claims =
+        Jwts.parserBuilder()
+            .setSigningKey(getSigningKey())
+            .build()
+            .parseClaimsJws(token)
+            .body
+
+
+    fun validateToken(token: String): JwtValidationType {
+        return try {
+            val claims = getBody(token)
+            JwtValidationType.VALID_JWT;
+        } catch (error: Exception) {
+            when (error) {
+                is MalformedJwtException -> JwtValidationType.INVALID_JWT_TOKEN
+                is ExpiredJwtException -> JwtValidationType.EXPIRED_JWT_TOKEN
+                is UnsupportedJwtException -> JwtValidationType.UNSUPPORTED_JWT_TOKEN
+                is IllegalArgumentException -> JwtValidationType.EMPTY_JWT
+                else -> JwtValidationType.INVALID_JWT_TOKEN
+            }
+        }
+    }
+
+    fun getUserFromJwt(token: String): Long {
+        val claims = getBody(token);
+        return claims[MEMBER_ID].toString().toLong()
+    }
+}
